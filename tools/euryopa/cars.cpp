6ce3c751767855bfd5b827cb8b6d476b7eb8c583
MatiasMontanna <matiasmontanna@hotmail.com>
2026-04-10 00:18:30 -0400

    cars.

--- tools/euryopa/cars.cpp
+++ tools/euryopa/cars.cpp
@@ -1,11 +1,9 @@
 #include "euryopa.h"
 #include "cars.h"
 #include "cdimage.h"
-#include <vector>
-
-namespace Cars {
 
 struct CarSpawn {
+	char iplName[32];
 	float x, y, z;
 	float angle;
 	int32 vehicleId;
@@ -14,19 +12,33 @@ struct CarSpawn {
 	int32 forceSpawn;
 	int32 alarmProb;
 	int32 lockedProb;
+	int32 unknown1;
+	int32 unknown2;
 };
 
 static std::vector<CarSpawn> carSpawns;
-static bool loaded = false;
 
-void Init(void)
+bool Cars::gRenderAsCubes = true;
+
+namespace Cars {
+
+void
+Init(void)
 {
-	if(loaded)
-		return;
+	log("Cars: Starting...\n");
 
-	carSpawns.clear();
+	if(!cdImages){
+		log("Cars: cdImages not initialized\n");
+		return;
+	}
 
 	CdImage *gta3img = &cdImages[0];
+	if(!gta3img || !gta3img->directory){
+		log("Cars: gta3.img not loaded\n");
+		return;
+	}
+
+	log("Cars: scanning gta3.img (%d entries)\n", gta3img->directorySize);
 
 	for(int i = 0; i < gta3img->directorySize; i++){
 		DirEntry *de = &gta3img->directory[i];
@@ -35,11 +47,16 @@ void Init(void)
 
 		int size = 0;
 		uint8 *buffer = ReadFileFromImage(i, &size);
-		if(!buffer || size < 0x4C)
+		if(!buffer || size < 0x4C){
+			if(buffer) free(buffer);
 			continue;
+		}
 
-		if(*(uint32*)buffer != 0x79726E62)
+		uint32 magic = *(uint32*)buffer;
+		if(magic != 0x79726E62){
+			free(buffer);
 			continue;
+		}
 
 		int32 numCars = *(int32*)(buffer + 0x10);
 		if(numCars <= 0){
@@ -52,6 +69,8 @@ void Init(void)
 			free(buffer);
 			continue;
 		}
+
+		log("Cars: %s has %d cars\n", de->name, numCars);
 
 		uint8 *carsData = buffer + carsOffset;
 		for(int j = 0; j < numCars; j++){
@@ -68,6 +87,10 @@ void Init(void)
 			spawn.forceSpawn = *(int32*)(carEntry + 28);
 			spawn.alarmProb = *(int32*)(carEntry + 32);
 			spawn.lockedProb = *(int32*)(carEntry + 36);
+			spawn.unknown1 = *(int32*)(carEntry + 40);
+			spawn.unknown2 = *(int32*)(carEntry + 44);
+			strncpy(spawn.iplName, de->name, sizeof(spawn.iplName) - 1);
+			spawn.iplName[sizeof(spawn.iplName) - 1] = '\0';
 
 			carSpawns.push_back(spawn);
 		}
@@ -75,20 +98,52 @@ void Init(void)
 		free(buffer);
 	}
 
-	log("Cars: found %d car spawns in gta3.img\n", carSpawns.size());
-	loaded = true;
+	log("Cars: loaded %d spawns\n", (int)carSpawns.size());
 }
 
-void Render(void)
+void
+Render(void)
 {
-	if(!loaded || carSpawns.empty())
+	if(carSpawns.empty())
 		return;
 
-	for(const auto& car : carSpawns){
-		DrawWireBox(car.x - 1.5f, car.y - 1.5f, car.z,
-		            car.x + 1.5f, car.y + 1.5f, car.z + 3.0f,
-		            0xFF00FF00);
+	if(!Cars::gRenderAsCubes)
+		return;
+
+	uint8 alpha = (uint8)(gCollisionWireframeAlpha * 255.0f);
+	rw::RGBA col = { 0, 255, 0, alpha };
+
+	for(size_t i = 0; i < carSpawns.size(); i++){
+		CarSpawn &car = carSpawns[i];
+
+		float halfX = 1.5f;
+		float halfY = 1.5f;
+		float halfZ = 1.5f;
+
+		rw::V3d v[8] = {
+			{ car.x - halfX, car.y - halfY, car.z },
+			{ car.x + halfX, car.y - halfY, car.z },
+			{ car.x - halfX, car.y + halfY, car.z },
+			{ car.x + halfX, car.y + halfY, car.z },
+			{ car.x - halfX, car.y - halfY, car.z + halfZ * 2.0f },
+			{ car.x + halfX, car.y - halfY, car.z + halfZ * 2.0f },
+			{ car.x - halfX, car.y + halfY, car.z + halfZ * 2.0f },
+			{ car.x + halfX, car.y + halfY, car.z + halfZ * 2.0f }
+		};
+
+		RenderLine(v[0], v[1], col, col);
+		RenderLine(v[2], v[3], col, col);
+		RenderLine(v[4], v[5], col, col);
+		RenderLine(v[6], v[7], col, col);
+		RenderLine(v[0], v[2], col, col);
+		RenderLine(v[1], v[3], col, col);
+		RenderLine(v[4], v[6], col, col);
+		RenderLine(v[5], v[7], col, col);
+		RenderLine(v[0], v[4], col, col);
+		RenderLine(v[1], v[5], col, col);
+		RenderLine(v[2], v[6], col, col);
+		RenderLine(v[3], v[7], col, col);
 	}
 }
 
-}
+}  // namespace Cars
--- tools/euryopa/cars.h
+++ tools/euryopa/cars.h
@@ -1,12 +1,27 @@
 #ifndef CARS_H
 #define CARS_H
 
-#include "euryopa.h"
+#include <vector>
+
+struct CarSpawn {
+	char iplName[32];
+	float x, y, z;
+	float angle;
+	int32 vehicleId;
+	int32 primaryColor;
+	int32 secondaryColor;
+	int32 forceSpawn;
+	int32 alarmProb;
+	int32 lockedProb;
+	int32 unknown1;
+	int32 unknown2;
+};
 
 namespace Cars {
 
 void Init(void);
 void Render(void);
+extern bool gRenderAsCubes;
 
 }
 
