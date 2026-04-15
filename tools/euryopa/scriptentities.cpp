c6bed1c14d6f5a3adecdcd1a429ffbbb24e82993
MatiasMontanna <matiasmontanna@hotmail.com>
2026-04-15 15:30:05 -0400

    test.

--- tools/euryopa/scriptentities.cpp
+++ tools/euryopa/scriptentities.cpp
@@ -244,8 +244,8 @@ ScriptEntities::TeleportToEntity(int entityIndex)
 void
 ScriptEntities::TeleportToCoords(float x, float y, float z)
 {
-	TheCamera.m_position = rw::V3d(x, y, z);
-	TheCamera.m_target = rw::V3d(x, y, z + 10.0f);
+	TheCamera.m_position = { x, y, z };
+	TheCamera.m_target = { x, y, z + 10.0f };
 }
 
 static void parseScFile(const char* filepath, const char* baseDir, const char* filename, std::map<std::string, float>& coordVars, int fileIndex)
